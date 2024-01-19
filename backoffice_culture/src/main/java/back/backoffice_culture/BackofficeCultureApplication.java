4b4b9c936460beeb95e654a08e3c0de412dafc12
Miandritiana <miandrititana21@gmail.com>
2024-01-19 11:58:16 +0300

    add folder springboot

---
+++ backoffice_culture/src/main/java/back/backoffice_culture/BackofficeCultureApplication.java
@@ -0,0 +1,13 @@
+package back.backoffice_culture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeCultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofficeCultureApplication.class, args);
+	}
+
+}
---
+++ backoffice_culture/src/test/java/back/backoffice_culture/BackofficeCultureApplicationTests.java
@@ -0,0 +1,13 @@
+package back.backoffice_culture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeCultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

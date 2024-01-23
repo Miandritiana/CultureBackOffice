4a40a9510c547d230b001cce7be8b5df9bd6632f
Henintsooa <120570194+Henintsooa@users.noreply.github.com>
2024-01-23 23:46:14 +0300

    Model Connexion

---
+++ backoffice_culture/src/main/java/back/backoffice_culture/Models/Connexion.java
@@ -0,0 +1,21 @@
+package back.backoffice_culture.Models;
+import java.sql.*;
+
+public class Connexion
+{
+	public static Connection getConnection()
+	{
+		try
+		{
+			Class.forName("org.postgresql.Driver");
+
+			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Culture","postgres","root");
+
+			return conn;
+		}
+		catch(Exception e){ 
+			System.out.println(e);
+		}
+		return null;
+	}
+}

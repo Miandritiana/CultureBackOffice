7184372021c90d805282e11af46e2210fb7856b2
Henintsooa <120570194+Henintsooa@users.noreply.github.com>
2024-01-23 23:44:23 +0300

    modif table useruser

--- backoffice_culture/Culture.sql
+++ backoffice_culture/Culture.sql
@@ -1,8 +1,11 @@
 CREATE TABLE useruser (
     iduser serial PRIMARY KEY,
     nomuser varchar(100),
-    password varchar(100)
+    password varchar(100),
+    idAdmin boolean default false
 );
+INSERT INTO useruser (nomuser, password, idAdmin) VALUES ('admin', '123', true);
+
 
 CREATE TABLE terrain (
     idterrain serial PRIMARY KEY,
@@ -34,9 +37,13 @@ CREATE TABLE categorieculture (
     nomcatecult varchar(100),
     prix double precision --/m2
 );
+INSERT INTO categorieculture (nomcatecult, prix) VALUES ('Fleurs', 20000);
+INSERT INTO categorieculture (nomcatecult, prix) VALUES ('Herbes aromatiques', 15000);
+
 
 CREATE TABLE parcelleculture (
     idpc serial PRIMARY KEY,
+    daty timestamp,
     idp integer references parcelle(idp),
     idcatecult integer references categorieculture(idcatecult),
     rendement integer --kg/m2

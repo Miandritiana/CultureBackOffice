acf78972f19bb764256a647a974ddf9773305218
Henintsooa <120570194+Henintsooa@users.noreply.github.com>
2024-01-26 14:54:45 +0300

    connexion base local

--- src/main/java/back/backoffice_culture/Models/Connexion.java
+++ src/main/java/back/backoffice_culture/Models/Connexion.java
@@ -9,7 +9,8 @@ public class Connexion
 		{
 			Class.forName("org.postgresql.Driver");
 
-			Connection conn = DriverManager.getConnection("jdbc:postgresql://viaduct.proxy.rlwy.net:54658/railway","postgres","G5d--2dEg5g5B1fag*Gc*1b-4B3Dfa2C");
+			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Culture","postgres","root");
+			// Connection conn = DriverManager.getConnection("jdbc:postgresql://viaduct.proxy.rlwy.net:54658/railway","postgres","G5d--2dEg5g5B1fag*Gc*1b-4B3Dfa2C");
 
 			return conn;
 		}

7a8449d6f3b5b53369425b88b6d8a21d8fd10319
Henintsooa <120570194+Henintsooa@users.noreply.github.com>
2024-01-29 16:51:03 +0300

    Update UserController.java

--- src/main/java/back/backoffice_culture/Controller/UserController.java
+++ src/main/java/back/backoffice_culture/Controller/UserController.java
@@ -1,10 +1,13 @@
 package back.backoffice_culture.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import back.backoffice_culture.Models.User;
 import back.backoffice_culture.Models.Connexion;
 import back.backoffice_culture.Models.User;
 
@@ -26,5 +29,21 @@ public class UserController {
         boolean isAdmin = user.isAdmin(c, idUser);
     
         return isAdmin;
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<String> insertUser(@RequestParam String username, @RequestParam String password) {
+        Connexion c = new Connexion();
+
+        try {
+            User User = new User(username, password);
+            User.insertUser(User, c);
+
+            return ResponseEntity.ok("User insérée avec succès");
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return ResponseEntity.status(500).body("Erreur lors de l'insertion de la parcelle : " + e.getMessage());
+        }
     }
 }

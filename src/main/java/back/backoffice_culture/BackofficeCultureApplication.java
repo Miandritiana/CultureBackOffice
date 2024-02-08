2b4992ab45edaf3921270237a382b1d297a874a6
Miandritiana <miandrititana21@gmail.com>
2024-02-08 17:03:43 +0300

    test search

--- src/main/java/back/backoffice_culture/BackofficeCultureApplication.java
+++ src/main/java/back/backoffice_culture/BackofficeCultureApplication.java
@@ -8,6 +8,7 @@ import com.mongodb.client.MongoClient;
 
 import back.backoffice_culture.Models.Parcelle;
 import back.backoffice_culture.Models.TerrainMobile;
+import back.backoffice_culture.Models.ViewListeCulture;
 import back.backoffice_culture.Models.ViewTerrainAValider;
 import back.backoffice_culture.Models.Connexion;
 import back.backoffice_culture.Models.DiscussionRepository;
@@ -21,26 +22,34 @@ public class BackofficeCultureApplication {
 		Connexion c = new Connexion();
 		System.out.println(c.getConnection());
 
-		Parcelle ccult = new Parcelle();
-		Parcelle[] l_cu = ccult.selectParcelle(c);
-		for (int i = 0; i < l_cu.length; i++) {
-			System.out.println(l_cu[i].getNomParcelle());
-		}
+		// Parcelle ccult = new Parcelle();
+		// Parcelle[] l_cu = ccult.selectParcelle(c);
+		// for (int i = 0; i < l_cu.length; i++) {
+		// 	System.out.println(l_cu[i].getNomParcelle());
+		// }
 
-		TerrainMobile[] terrains = TerrainMobile.selectTerrainsById(c,3);
+		// TerrainMobile[] terrains = TerrainMobile.selectTerrainsById(c,3);
 
 
-        if (terrains != null) {
-            for (TerrainMobile terrain : terrains) {
-                System.out.println("ID Terrain : " + terrain.getIdTerrain());
-                System.out.println("Description : " + terrain.getDescription());
-                System.out.println("ID User : " + terrain.getIdUser());
-                System.out.println("Nom User : " + terrain.getNomUser());
-                System.out.println("Photo : " + terrain.getPhoto());
-                System.out.println("------------------------------------------");
-            }
-        } else {
-            System.out.println("Aucun terrain à valider trouvé.");
+        // if (terrains != null) {
+        //     for (TerrainMobile terrain : terrains) {
+        //         System.out.println("ID Terrain : " + terrain.getIdTerrain());
+        //         System.out.println("Description : " + terrain.getDescription());
+        //         System.out.println("ID User : " + terrain.getIdUser());
+        //         System.out.println("Nom User : " + terrain.getNomUser());
+        //         System.out.println("Photo : " + terrain.getPhoto());
+        //         System.out.println("------------------------------------------");
+        //     }
+        // } else {
+        //     System.out.println("Aucun terrain à valider trouvé.");
+        // }
+
+        ViewListeCulture test = new ViewListeCulture();
+        ViewListeCulture[] l = test.selectViewListeCulture(c, 0, "Rasoa");
+        for (ViewListeCulture vv : l)
+        {
+            System.out.println(vv.getNomCulture());
+            System.out.println(vv.getNomUser());
         }
 
         
